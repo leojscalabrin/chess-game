70d4e8156fea8f7c97deb3d0a61afc05d5601a0d
Leonardo Scalabrin <leojscalabrin@gmail.com>
2022-09-18 12:04:23 -0300

    tabuleiro

---
+++ chess-app/src/components/Chessboard.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import "./Chessboard.css";
+
+const verticalAxis = [1, 2, 3, 4, 5, 6, 7, 8]
+const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"]
+
+export default function Chessboard() {
+  return <div id ="chessboard"></div>;
+}
