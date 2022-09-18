22005f85f35a2ebfe3393861f37fb266cdb7ede7
Leonardo Scalabrin <leojscalabrin@gmail.com>
2022-09-18 12:16:08 -0300

    campos

--- chess-app/src/components/Chessboard.tsx
+++ chess-app/src/components/Chessboard.tsx
@@ -2,9 +2,20 @@ import React from "react";
 
 import "./Chessboard.css";
 
-const verticalAxis = [1, 2, 3, 4, 5, 6, 7, 8]
-const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"]
+const verticalAxis = [1, 2, 3, 4, 5, 6, 7, 8];
+const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
 export default function Chessboard() {
-  return <div id ="chessboard"></div>;
+  let board = [];
+
+  for (let j = verticalAxis.length - 1; j >= 0; j--) {
+    for (let i = 0; i < horizontalAxis.length; i++) {
+      board.push(
+        <div className="tile">
+          {horizontalAxis[i]} {verticalAxis[j]}
+        </div>
+      );
+    }
+  }
+  return <div id="chessboard">{board}</div>;
 }
