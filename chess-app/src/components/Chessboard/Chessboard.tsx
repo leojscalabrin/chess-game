290027ea2f450fa997c2c97c474d9e06f819a3b8
Leonardo Scalabrin <leojscalabrin@gmail.com>
2023-05-10 10:13:26 -0300

    chave das peças

--- chess-app/src/components/Chessboard/Chessboard.tsx
+++ chess-app/src/components/Chessboard/Chessboard.tsx
@@ -56,7 +56,7 @@ export default function Chessboard() {
         }
       });
 
-      board.push(<Tile image={image} number={number} />);
+      board.push(<Tile key={`${j}, ${i}`} image={image} number={number} />);
     }
   }
   return <div id="chessboard">{board}</div>;
