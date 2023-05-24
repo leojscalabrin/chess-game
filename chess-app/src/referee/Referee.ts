c6ca1cd98b8f6f3e038808c7703099f496d6f491
Leonardo Scalabrin <leojscalabrin@gmail.com>
2023-05-24 11:52:25 -0300

    logica do peao

--- chess-app/src/referee/Referee.ts
+++ chess-app/src/referee/Referee.ts
@@ -4,7 +4,14 @@ export default class Referee {
     isValidMove(px:number, py:number, x:number, y:number, type:PieceType, team: TeamType) {
 
         if(type === PieceType.PAWN) {
-
+            if(team === TeamType.OUR) {
+                if (py === 1) {
+                    if(px === x && (y - py === 1 || y - py === 2)) {
+                        console.log("Movimento válido")
+                        return true;
+                    }
+                }
+            }
         }
 
         return true;
