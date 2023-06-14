86c6f1dfa8acc3dcec247fb755ec3bfccdb5218b
Leonardo Scalabrin <leojscalabrin@gmail.com>
2023-06-14 10:59:47 -0300

    func tile is occupied

--- chess-app/src/components/Chessboard/Chessboard.tsx
+++ chess-app/src/components/Chessboard/Chessboard.tsx
@@ -5,17 +5,17 @@ import Referee from "../../referee/Referee";
 
 const verticalAxis = [1, 2, 3, 4, 5, 6, 7, 8];
 const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
-interface Piece {
+export interface Piece {
   image: string;
   x: number;
   y: number;
-  type: PieceType,
-  team: TeamType
+  type: PieceType;
+  team: TeamType;
 }
 
 export enum TeamType {
   OPPONENT,
-  OUR
+  OUR,
 }
 
 export enum PieceType {
@@ -24,33 +24,93 @@ export enum PieceType {
   KNIGHT,
   ROOK,
   QUEEN,
-  KING
+  KING,
 }
 
 const initialBoardState: Piece[] = [];
 
 // estado inicial das peças
 for (let p = 0; p < 2; p++) {
-  const teamType = (p === 0) ? TeamType.OPPONENT : TeamType.OUR;
-  const type = (teamType === TeamType.OPPONENT) ? "b" : "w";
-  const y = (teamType === TeamType.OPPONENT) ? 7 : 0;
+  const teamType = p === 0 ? TeamType.OPPONENT : TeamType.OUR;
+  const type = teamType === TeamType.OPPONENT ? "b" : "w";
+  const y = teamType === TeamType.OPPONENT ? 7 : 0;
 
-  initialBoardState.push({ image: `assets/images/rook_${type}.png`, x: 0, y, type: PieceType.ROOK, team: teamType });
-  initialBoardState.push({ image: `assets/images/rook_${type}.png`, x: 7, y, type: PieceType.ROOK, team: teamType });
-  initialBoardState.push({ image: `assets/images/knight_${type}.png`, x: 1, y, type: PieceType.KNIGHT, team: teamType });
-  initialBoardState.push({ image: `assets/images/knight_${type}.png`, x: 6, y, type: PieceType.KNIGHT, team: teamType });
-  initialBoardState.push({ image: `assets/images/bishop_${type}.png`, x: 2, y, type: PieceType.BISHOP, team: teamType });
-  initialBoardState.push({ image: `assets/images/bishop_${type}.png`, x: 5, y, type: PieceType.BISHOP, team: teamType });
-  initialBoardState.push({ image: `assets/images/queen_${type}.png`, x: 3, y, type: PieceType.QUEEN, team: teamType });
-  initialBoardState.push({ image: `assets/images/king_${type}.png`, x: 4, y, type: PieceType.KING, team: teamType });
+  initialBoardState.push({
+    image: `assets/images/rook_${type}.png`,
+    x: 0,
+    y,
+    type: PieceType.ROOK,
+    team: teamType,
+  });
+  initialBoardState.push({
+    image: `assets/images/rook_${type}.png`,
+    x: 7,
+    y,
+    type: PieceType.ROOK,
+    team: teamType,
+  });
+  initialBoardState.push({
+    image: `assets/images/knight_${type}.png`,
+    x: 1,
+    y,
+    type: PieceType.KNIGHT,
+    team: teamType,
+  });
+  initialBoardState.push({
+    image: `assets/images/knight_${type}.png`,
+    x: 6,
+    y,
+    type: PieceType.KNIGHT,
+    team: teamType,
+  });
+  initialBoardState.push({
+    image: `assets/images/bishop_${type}.png`,
+    x: 2,
+    y,
+    type: PieceType.BISHOP,
+    team: teamType,
+  });
+  initialBoardState.push({
+    image: `assets/images/bishop_${type}.png`,
+    x: 5,
+    y,
+    type: PieceType.BISHOP,
+    team: teamType,
+  });
+  initialBoardState.push({
+    image: `assets/images/queen_${type}.png`,
+    x: 3,
+    y,
+    type: PieceType.QUEEN,
+    team: teamType,
+  });
+  initialBoardState.push({
+    image: `assets/images/king_${type}.png`,
+    x: 4,
+    y,
+    type: PieceType.KING,
+    team: teamType,
+  });
 }
 // peões pretos
 for (let i = 0; i < 8; i++) {
-  initialBoardState.push({ image: "assets/images/pawn_b.png", x: i, y: 6, type: PieceType.PAWN, team: TeamType.OPPONENT });
+  initialBoardState.push({
+    image: "assets/images/pawn_b.png",
+    x: i,
+    y: 6,
+    type: PieceType.PAWN,
+    team: TeamType.OPPONENT,
+  });
 }
 // peões brancos
 for (let i = 0; i < 8; i++) {
-  initialBoardState.push({ image: "assets/images/pawn_w.png", x: i, y: 1, type: PieceType.PAWN, team: TeamType.OUR });
+  initialBoardState.push({
+    image: "assets/images/pawn_w.png",
+    x: i,
+    y: 1,
+    type: PieceType.PAWN,
+    team: TeamType.OUR,
+  });
 }
 
 // criando as áreas do tabuleiro
@@ -68,7 +128,9 @@ export default function Chessboard() {
     const chessboard = chessboardRef.current;
     if (element.classList.contains("chess-piece") && chessboard) {
       setGridX(Math.floor((e.clientX - chessboard.offsetLeft) / 100));
-      setGridY(Math.abs(Math.ceil((e.clientY - chessboard.offsetTop - 800) / 100)));
+      setGridY(
+        Math.abs(Math.ceil((e.clientY - chessboard.offsetTop - 800) / 100))
+      );
 
       const x = e.clientX - 50;
       const y = e.clientY - 50;
@@ -117,28 +179,37 @@ export default function Chessboard() {
     const chessboard = chessboardRef.current;
     if (activePiece && chessboard) {
       const x = Math.floor((e.clientX - chessboard.offsetLeft) / 100);
-      const y = Math.abs(Math.ceil((e.clientY - chessboard.offsetTop - 800) / 100));
+      const y = Math.abs(
+        Math.ceil((e.clientY - chessboard.offsetTop - 800) / 100)
+      );
 
       //atualiza a posição da peça
-      setPieces(value => {
-        const pieces = value.map(p => {
-          if(p.x === gridX && p.y === gridY) {
-            const validMove = referee.isValidMove(gridX, gridY, x, y, p.type, p.team);
+      setPieces((value) => {
+        const pieces = value.map((p) => {
+          if (p.x === gridX && p.y === gridY) {
+            const validMove = referee.isValidMove(
+              gridX,
+              gridY,
+              x,
+              y,
+              p.type,
+              p.team,
+              value
+            );
 
-            if(validMove) {
+            if (validMove) {
               p.x = x;
               p.y = y;
             } else {
-              activePiece.style.position = 'relative';
-              activePiece.style.removeProperty('top');
-              activePiece.style.removeProperty('left');
+              activePiece.style.position = "relative";
+              activePiece.style.removeProperty("top");
+              activePiece.style.removeProperty("left");
             }
-
           }
           return p;
-        })
+        });
         return pieces;
-      })
+      });
       setActivePiece(null);
     }
   }
--- chess-app/src/referee/Referee.ts
+++ chess-app/src/referee/Referee.ts
@@ -1,13 +1,28 @@
-import { PieceType, TeamType } from "../components/Chessboard/Chessboard";
+import {
+  PieceType,
+  TeamType,
+  Piece,
+} from "../components/Chessboard/Chessboard";
 
 export default class Referee {
+  tileIsOccupied(x: number, y: number, boardState: Piece[]): boolean {
+
+    const piece = boardState.find((p) => p.x === x && p.y === y);
+    if (piece) {
+        return true;
+    } else {
+        return false;
+    }
+  }
+
   isValidMove(
     px: number,
     py: number,
     x: number,
     y: number,
     type: PieceType,
-    team: TeamType
+    team: TeamType,
+    boardState: Piece[]
   ) {
     // lógica para o peão
     if (type === PieceType.PAWN) {
@@ -15,25 +30,28 @@ export default class Referee {
         // checando se é o primeiro movimento
         if (py === 1) {
           if (px === x && (y - py === 1 || y - py === 2)) {
-            console.log("Movimento válido");
-            return true;
+            if (!this.tileIsOccupied(x, y, boardState)) {
+                return true;
+            }
           }
         } else {
           // movimento comum
           if (px === x && y - py === 1) {
-            return true;
+            if (!this.tileIsOccupied(x, y, boardState)) {
+                return true;
+            } 
           }
         }
+        // movimento do peão preto
       } else {
         if (py === 6) {
           if (px === x && (y - py === -1 || y - py === -2)) {
-            console.log("Movimento válido");
             return true;
           }
         } else {
-            if(px === x && y - py === -1) {
-                return true;
-            }
+          if (px === x && y - py === -1) {
+            return true;
+          }
         }
       }
     }
