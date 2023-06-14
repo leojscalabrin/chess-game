78f9e10b7377733d1af9ee2e36e4deb085129318
Leonardo Scalabrin <leojscalabrin@gmail.com>
2023-06-14 10:50:39 -0300

    movimento do peao preto

--- chess-app/src/referee/Referee.ts
+++ chess-app/src/referee/Referee.ts
@@ -1,25 +1,42 @@
 import { PieceType, TeamType } from "../components/Chessboard/Chessboard";
 
 export default class Referee {
-    isValidMove(px:number, py:number, x:number, y:number, type:PieceType, team: TeamType) {
-        // lógica para o peão
-        if(type === PieceType.PAWN) {
-            if(team === TeamType.OUR) {
-                // checando se é o primeiro movimento
-                if (py === 1) {
-                    if(px === x && (y - py === 1 || y - py === 2)) {
-                        console.log("Movimento válido")
-                        return true;
-                    }
-                }
-            } else {
-                // movimento comum
-                if(px === x && y - py === 1) {
-                    return true;
-                }
+  isValidMove(
+    px: number,
+    py: number,
+    x: number,
+    y: number,
+    type: PieceType,
+    team: TeamType
+  ) {
+    // lógica para o peão
+    if (type === PieceType.PAWN) {
+      if (team === TeamType.OUR) {
+        // checando se é o primeiro movimento
+        if (py === 1) {
+          if (px === x && (y - py === 1 || y - py === 2)) {
+            console.log("Movimento válido");
+            return true;
+          }
+        } else {
+          // movimento comum
+          if (px === x && y - py === 1) {
+            return true;
+          }
+        }
+      } else {
+        if (py === 6) {
+          if (px === x && (y - py === -1 || y - py === -2)) {
+            console.log("Movimento válido");
+            return true;
+          }
+        } else {
+            if(px === x && y - py === -1) {
+                return true;
             }
         }
-
-        return false;
+      }
     }
+    return false;
+  }
 }
