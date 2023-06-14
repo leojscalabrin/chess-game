8b965aa8054c4caed0b8355d0b9f448214755324
Leonardo Scalabrin <leojscalabrin@gmail.com>
2023-06-14 11:20:36 -0300

    refatorando código dos peões

--- chess-app/src/referee/Referee.ts
+++ chess-app/src/referee/Referee.ts
@@ -6,12 +6,11 @@ import {
 
 export default class Referee {
   tileIsOccupied(x: number, y: number, boardState: Piece[]): boolean {
-
     const piece = boardState.find((p) => p.x === x && p.y === y);
     if (piece) {
-        return true;
+      return true;
     } else {
-        return false;
+      return false;
     }
   }
 
@@ -24,49 +23,24 @@ export default class Referee {
     team: TeamType,
     boardState: Piece[]
   ) {
-    // lógica para o peão
     if (type === PieceType.PAWN) {
-      if (team === TeamType.OUR) {
-        // checando se é o primeiro movimento
-        if (py === 1) {
-          if (px === x && y - py === 1) {
-            if (!this.tileIsOccupied(x, y, boardState)) {
-                return true;
-            }
-          } else if (px === x && y - py === 2) {
-            if (!this.tileIsOccupied(x, y, boardState) && !this.tileIsOccupied(x, y-1, boardState)){
-                return true;
-            }
+      const specialRow = team === TeamType.OUR ? 1 : 6;
+      const pawnDirection = team === TeamType.OUR ? 1 : -1;
+
+      if(px === x && py === specialRow && y - py === 2*pawnDirection) {
+        if (
+            !this.tileIsOccupied(x, y, boardState) &&
+            !this.tileIsOccupied(x, y - pawnDirection, boardState)
+          ) {
+            return true;
           }
-        } else {
-          // movimento comum
-          if (px === x && y - py === 1) {
-            if (!this.tileIsOccupied(x, y, boardState)) {
-                return true;
-            } 
+      } else if (px === x && y - py === pawnDirection ) {
+        if (!this.tileIsOccupied(x, y, boardState)) {
+            return true;
           }
-        }
-        // movimento do peão preto
-      } else {
-        if (py === 6) {
-          if (px === x && y - py === -1) {
-            if (!this.tileIsOccupied(x, y, boardState)) { 
-                return true;
-            }
-          } else if(px === x && y - py === -2) {
-            if (!this.tileIsOccupied(x, y, boardState) && !this.tileIsOccupied(x, y+1, boardState) ) { 
-                return true;
-            }
-          }
-        } else {
-          if (px === x && y - py === -1) {
-            if(!this.tileIsOccupied(x, y, boardState)) {
-                return true;
-            }
-          }
-        }
       }
     }
+
     return false;
   }
 }
